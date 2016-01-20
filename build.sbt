281bf099495f155850967341a2cb0552d9ec56ac
Kevin Schuetz <schuetzk@gmail.com>
2016-01-19 22:13:23 -0600

    Updated ExportMidiFile to use newer scodec

--- build.sbt
+++ build.sbt
@@ -8,14 +8,14 @@ description := "Scala port of Euterpea Haskell music library"
 
 licenses += ("BSD Simplified", url("http://opensource.org/licenses/bsd-license"))
 
-scalaVersion := "2.11.5"
+scalaVersion := "2.11.7"
 
 incOptions := incOptions.value.withNameHashing(true)
 
 libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-effect" % "7.1.0",
-  "org.spire-math" %% "spire" % "0.9.0",
-  "org.scodec" %% "scodec-bits" % "1.0.5",
-  "org.typelevel" %% "scodec-core" % "1.6.0"
+  "org.scalaz" %% "scalaz-effect" % "7.1.6",
+  "org.spire-math" %% "spire" % "0.11.0",
+  "org.scodec" %% "scodec-bits" % "1.0.12",
+  "org.scodec" %% "scodec-core" % "1.8.3"
 )
 
--- src/main/scala/euterpea/io/midi/ExportMidiFile.scala
+++ src/main/scala/euterpea/io/midi/ExportMidiFile.scala
@@ -2,6 +2,7 @@ package euterpea.io.midi
 
 import java.nio.file.{Paths, Files}
 
+import scodec.{Err, Attempt}
 import scodec.bits._
 import scodec.codecs._
 import scodec.midi.Midi.Message._
@@ -11,72 +12,127 @@ import scalaz.effect._
 import scalaz.std.effect.closeable._
 
 object ExportMidiFile {
-  def makeFile(midi: Midi): ByteVector = {
+  def makeFile(midi: Midi): Attempt[ByteVector] = {
     println(midi)
     val ticksPerQn = midi.timeDiv match {
       case TicksPerBeat(x) => x
       case TicksPerSecond(_, _) => sys.error("(makeFile) Don't know how to handle TicksPerSecond yet.")
     }
     val header = makeHeader(midi.fileType, midi.tracks.length, ticksPerQn)
-    val body = ByteVector.concat(midi.tracks.map(makeTrack))
-    header ++ body
+    val body = midi.tracks.foldLeft(Attempt.successful(List.empty[ByteVector])) {
+      case (Attempt.Successful(res), track) => makeTrack(track).map(_ :: res)
+      case (failure, _) => failure
+    } map { t =>
+      ByteVector.concat(t.reverse)
+    }
+
+    for {
+      h <- header
+      b <- body
+    } yield h ++ b
   }
-  val midiHeaderConst = ascii.encodeValid("MThd") ++ uint32.encodeValid(6L)
+  val midiHeaderConst = for {
+    h1 <- ascii.encode("MThd")
+    h2 <- uint32.encode(6L)
+  } yield h1 ++ h2
+
   type TrackCount = Int
   type TicksPerQN = Int
-  def makeHeader(ft: FileType, numTracks: TrackCount, ticksPerQn: TicksPerQN): ByteVector = {
+  def makeHeader(ft: FileType, numTracks: TrackCount, ticksPerQn: TicksPerQN): Attempt[ByteVector] = {
     val ftp = ft match {
       case SingleTrack => hex"0000".bits
       case MultiTrack => hex"0001".bits
       case MultiPattern => sys.error("(makeHeader) don't know how to handle multi-pattern yet.")
     }
-    val numTracksp = uint16.encodeValid(numTracks)
-    val ticksPerQNp = uint16.encodeValid(ticksPerQn)
-    (midiHeaderConst ++ ftp ++ numTracksp ++ ticksPerQNp).toByteVector
-  }
-  def makeTrack(t: Track[Ticks]): ByteVector = {
-    val body = makeTrackBody(t)
-    val header = makeTrackHeader(body)
-    header ++ body
-  }
-  val trackHeaderConst = ascii.encodeValid("MTrk")
-  def makeTrackHeader(tBody: ByteVector): ByteVector = {
-    (trackHeaderConst ++ uint32.encodeValid(tBody.length)).toByteVector
-  }
-  def makeTrackBody(t: Track[Ticks]): ByteVector = t match {
-    case Nil => endOfTrack
-    case (ticks, msg) :: rest =>
-      val b = msgToBytes(msg)
-      if (b.length > 0) {
-        val bp = List(to7Bits(ticks), b, makeTrackBody(rest))
-        ByteVector.concat(bp)
-      }
-      else
-        makeTrackBody(rest)
-  }
-  val endOfTrack = to7Bits(96) ++ hex"FF2F00"
-  def to7Bits(i: Long): ByteVector = {
-    if (i == 0L)
-      hex"00"
-    else {
-      val bits = int64.encodeValid(i)
-      val trimmed = bits.drop(bits.indexOfSlice(BitVector.one))
-      val length = trimmed.length
-      val paddedLength = ((length+6) / 7)*7
-      val padded = trimmed.padLeft(paddedLength)
-      val vec = padded.grouped(7).map(BitVector.one ++ _).toVector
-      val (bytes, Vector(last)) = vec.splitAt(vec.length - 1)
-      BitVector.concat(bytes :+ last.clear(0)).toByteVector
+    for {
+      hc <- midiHeaderConst
+      numTracksp <- uint16.encode(numTracks)
+      ticksPerQNp <- uint16.encode(ticksPerQn)
+    } yield {
+      (hc ++ ftp ++ numTracksp ++ ticksPerQNp).toByteVector
     }
   }
-  def msgToBytes(m: Message): ByteVector = m match {
-    case NoteOn(c, k, v) => ((hex"90".bits | uint8.encodeValid(c)) ++ uint8.encodeValid(k) ++ uint8.encodeValid(v)).toByteVector
-    case NoteOff(c, k, v) => ((hex"80".bits | uint8.encodeValid(c)) ++ uint8.encodeValid(k) ++ uint8.encodeValid(v)).toByteVector
-    case ProgramChange(c, p) => ((hex"c0".bits | uint8.encodeValid(c)) ++ uint8.encodeValid(p)).toByteVector
-    case ControlChange(c, n, v) => ((hex"b0".bits | uint8.encodeValid(c)) ++ uint8.encodeValid(n) ++ uint8.encodeValid(v)).toByteVector
-    case TempoChange(t) => (hex"ff5103".bits ++ uint24.encodeValid(t)).toByteVector
+
+  def makeTrack(t: Track[Ticks]): Attempt[ByteVector] = for {
+    body <- makeTrackBody(t)
+    header <- makeTrackHeader(body)
+  } yield header ++ body
+
+  val trackHeaderConst = ascii.encode("MTrk")
+  def makeTrackHeader(tBody: ByteVector): Attempt[ByteVector] = for {
+    thc <- trackHeaderConst
+    len <- uint32.encode(tBody.length)
+  } yield (thc ++ len).toByteVector
+
+  def makeTrackBody(t: Track[Ticks]): Attempt[ByteVector] = t match {
+    case Nil => endOfTrack
+    case (ticks, msg) :: rest =>
+      msgToBytes(msg).flatMap { b =>
+        if (b.length > 0) {
+          for {
+            t <- to7Bits(ticks)
+            more <- makeTrackBody(rest)
+          } yield ByteVector.concat(t :: b :: more :: Nil)
+        }
+        else
+          makeTrackBody(rest)
+      }
   }
-  def exportMidiFile(filePath: String, midi: Midi): IO[Unit] = writeFile(filePath, makeFile(midi))
+
+  val endOfTrack = to7Bits(96).map(_ ++ hex"FF2F00")
+  def to7Bits(i: Long): Attempt[ByteVector] = {
+    if (i == 0L)
+      Attempt.successful(hex"00")
+    else {
+      int64.encode(i).map { bits =>
+        val trimmed = bits.drop(bits.indexOfSlice(BitVector.one))
+        val length = trimmed.length
+        val paddedLength = ((length+6) / 7)*7
+        val padded = trimmed.padLeft(paddedLength)
+        val vec = padded.grouped(7).map(BitVector.one ++ _).toVector
+        val (bytes, Vector(last)) = vec.splitAt(vec.length - 1)
+        BitVector.concat(bytes :+ last.clear(0)).toByteVector
+      }
+    }
+  }
+
+  private val msgNoteOn = hex"90".bits
+  private val msgNoteOff = hex"80".bits
+  private val msgProgramChange = hex"c0".bits
+  private val msgControlChange = hex"b0".bits
+  private val msgTempoChange = hex"ff5103".bits
+
+  def msgToBytes(m: Message): Attempt[ByteVector] = m match {
+    case NoteOn(c, k, v) => for {
+      cb <- uint8.encode(c)
+      kb <- uint8.encode(k)
+      vb <- uint8.encode(v)
+    } yield ((msgNoteOn | cb) ++ kb ++ vb).toByteVector
+
+    case NoteOff(c, k, v) => for {
+      cb <- uint8.encode(c)
+      kb <- uint8.encode(k)
+      vb <- uint8.encode(v)
+    } yield ((msgNoteOff | cb) ++ kb ++ vb).toByteVector
+
+    case ProgramChange(c, p) => for {
+      cb <- uint8.encode(c)
+      pb <- uint8.encode(p)
+    } yield ((msgProgramChange | cb) ++ pb).toByteVector
+
+    case ControlChange(c, n, v) => for {
+      cb <- uint8.encode(c)
+      nb <- uint8.encode(n)
+      vb <- uint8.encode(v)
+    } yield ((msgNoteOff | cb) ++ nb ++ vb).toByteVector
+
+    case TempoChange(t) => for {
+      tb <- uint24.encode(t)
+    } yield (msgTempoChange ++ tb).toByteVector
+
+    case other => Attempt.failure(Err(s"Message unsupported: $other"))
+  }
+  def exportMidiFile(filePath: String, midi: Midi): IO[Unit] = writeFile(filePath, makeFile(midi).require)
   def writeFile(fileName: String, bv: ByteVector): IO[Unit] = IO {
     Files.newOutputStream(Paths.get(fileName))
   }.using(os => IO(bv.copyToStream(os)))
